9f58384789f533fba07e4d04c864737cf8ba25b8
seraphy <seraphy@seraphyware.jp>
2018-03-14 19:46:48 +0900

    iconのsvg変換

---
+++ etc/make_icons.bat
@@ -0,0 +1,10 @@
+set PATH=C:\Program Files\Inkscape;%PATH%
+inkscape -w 16 -h 16 -f circular.svg -e circular16.png
+inkscape -w 24 -h 24 -f circular.svg -e circular24.png
+inkscape -w 32 -h 32 -f circular.svg -e circular32.png
+inkscape -w 48 -h 48 -f circular.svg -e circular48.png
+inkscape -w 64 -h 64 -f circular.svg -e circular64.png
+inkscape -w 128 -h 128 -f circular.svg -e circular128.png
+inkscape -w 256 -h 256 -f circular.svg -e circular256.png
+pause
+
