b30e2a286273c095fb6a567fc4a0556571f00074
seraphy <seraphy@users.osdn.me>
2019-03-06 15:42:43 +0900

    Java11でビルド後にJava8以降で実行できるパッケージングに変更

--- src/assembly/run.bat
+++
@@ -1,4 +0,0 @@
-if "%JAVA_HOME%" == "" set JAVA_HOME=C:\Java\jdk1.8.0_121
-
-set PATH=%JAVA_HOME%\bin;%SystemRoot%\System32;%SystemRoot%
-java -jar ${project.artifactId}-${project.version}.jar
--- src/assembly/run11.bat
+++
@@ -1,4 +0,0 @@
-if "%JAVA_HOME%" == "" set JAVA_HOME=C:\Java\jdk-11
-
-set PATH=%JAVA_HOME%\bin;%SystemRoot%\System32;%SystemRoot%
-java -p lib --add-modules javafx.controls,javafx.fxml,javax.inject -jar ${project.artifactId}-${project.version}.jar
